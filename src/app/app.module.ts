c52a2a8ad35d36415b47ee8d6aade8c1026ea643
Robbie106gti <rleeuwerink@gmail.com>
2018-11-09 16:12:51 -0800

    basic setup init for v7 upgrade and CDK with Materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './ui/header/header.component';
+import { FooterComponent } from './ui/footer/footer.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { DemoMaterialModule } from './material-module';
+import { SliderComponent } from './ui/slider/slider.component';
+// guards
+import * as fromGuards from './guards';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    })
   ],
-  providers: [],
+  providers: [...fromGuards.guards],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/ui/slider/slider.component.ts
@@ -0,0 +1,23 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  AfterViewInit,
+  ElementRef,
+  ViewChild
+} from '@angular/core';
+declare var M: any;
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss']
+})
+export class SliderComponent implements AfterViewInit {
+  @ViewChild('slider', { read: ElementRef })
+  slider: ElementRef;
+
+  constructor() {}
+  ngAfterViewInit(): void {
+    M.Slider.init(this.slider.nativeElement, {});
+  }
+}
