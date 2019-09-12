23f7d56eb1ac1c56ecb3c62fe1022d0a20792648
robbie106gti <rleeuwerink@gmail.com>
2019-09-12 12:31:42 -0700

    two more end points for doorselector

--- src/api/src/routes/ds_view.php
+++ src/api/src/routes/ds_view.php
@@ -2,6 +2,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+// Get all Doors Custom-line for door selector
 $app->get('/ds/doors', function(Request $request, Response $response) {
     $sql = "SELECT * FROM doorstyle_data WHERE Custom = 1";
 
@@ -18,7 +19,44 @@ $app->get('/ds/doors', function(Request $request, Response $response) {
         echo json_encode('{"Error": {"text": '.$e->getMessage().'}');
     }
 });
-// Get all SOPs items
+
+// Get Colors for door selector
+$app->get('/ds/colors', function(Request $request, Response $response) {
+    $sql = "SELECT * FROM material_data WHERE visible = '1'";
+
+    try {
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $items = $stmt->fetchAll(PDO::FETCH_OBJ);
+        echo json_encode($items);
+    } catch(PDOException $e){
+        echo json_encode('{"Error": {"text": '.$e->getMessage().'}');
+    }
+});
+
+// Get Stains for door selector
+$app->get('/ds/stains', function(Request $request, Response $response) {
+    $sql = "SELECT * FROM stains_data WHERE visible = '1'";
+
+    try {
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $items = $stmt->fetchAll(PDO::FETCH_OBJ);
+        echo json_encode($items);
+    } catch(PDOException $e){
+        echo json_encode('{"Error": {"text": '.$e->getMessage().'}');
+    }
+});
+
+// Get Sections for door selector
 $app->get('/ds/{query}', function(Request $request, Response $response) {
   $query = $request->getAttribute('query');
     $sql = "SELECT * FROM doorselector WHERE type = '$query'";
